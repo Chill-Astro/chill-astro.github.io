9e7a69186ff7c80f17932d04f83dd8cbae72cd61
Dev. Chill-Astro <MasterArc3435@outlook.com>
2026-01-31 00:50:39 +0530

    Add 'FOSS-Root-Checker' to allowed repos list

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // List of specific repos to show
     const allowedRepos = [
         'Lamina',
+        'FOSS-Root-Checker',
         'PyCalc-GUI',
         'PyCalc',
         'Net-Update',
